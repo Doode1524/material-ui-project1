0a3ab832a1c5b37eb9d19c55e9e82c37d10b7a8a
joey <doode1524@gmail.com>
2021-04-21 01:23:19 -0400

    appbar init

---
+++ src/components/ui/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+
+const Header = (props) => {
+    return (
+       <AppBar position="fixed">
+           <Toolbar>2-Slice-Gaming</Toolbar>
+       </AppBar>
+    )
+}
+
+export default Header
