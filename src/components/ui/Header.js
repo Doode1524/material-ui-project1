a4deb874485030edd7e00a281b8b62397fb85fb3
joey <doode1524@gmail.com>
2021-04-23 19:13:56 -0400

    add free estimate button

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -5,6 +5,7 @@ import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import { makeStyles } from '@material-ui/styles'
 import Tabs from "@material-ui/core/Tabs"
 import Tab from "@material-ui/core/Tab"
+import Button from "@material-ui/core/Button"
 
 import logo from '../../assets/logo.svg'
 
@@ -35,6 +36,13 @@ const useStyles = makeStyles(theme => ({
     ...theme.typography.tab,
     minWidth: 10,
     marginLeft: "25px"
+  },
+  button: {
+    ...theme.typography.estimate,
+    borderRadius: "50px",
+    marginLeft: "50px",
+    marginRight: "25px",
+    height: "45px",
   }
 }))
 
@@ -54,6 +62,13 @@ const Header = (props) => {
                     <Tab className={classes.tab} label="About Us"></Tab>
                     <Tab className={classes.tab} label="Contact Us"></Tab>
                   </Tabs>
+                  <Button 
+                    variant="contained" 
+                    color="secondary"
+                    className={classes.button}
+                  >
+                      Free Estimate
+                  </Button>
                 </Toolbar>
             </AppBar>
         </ElevationScroll>
--- src/components/ui/Theme.js
+++ src/components/ui/Theme.js
@@ -22,6 +22,12 @@ export default createMuiTheme({
             textTransform: 'none',
             fontWeight: 700,
             fontSize: "1rem",
+        },
+        estimate: {
+            fontFamily: "Pacifico",
+            fontSize: "1rem",
+            textTransform: "none", // removes elements from default text
+            color: "white"
         }
     }
 })
