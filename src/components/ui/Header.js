bb32788d5a68d98d705d0321449f644fcced0e55
joey <doode1524@gmail.com>
2021-04-23 16:42:20 -0400

    header logo

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
-import Typography from '@material-ui/core/Typography'
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import { makeStyles } from '@material-ui/styles'
+
+import logo from '../../assets/logo.svg'
 
 function ElevationScroll(props) {
     const { children } = props;
@@ -19,20 +20,23 @@ function ElevationScroll(props) {
 
 const useStyles = makeStyles(theme => ({
   toolbarMargin: {
-    ...theme.mixins.toolbar
+    ...theme.mixins.toolbar,
+    marginBottom: "3em"
+  },
+  logo: {
+    height: "7em"
   }
 }))
 
 const Header = (props) => {
   const classes = useStyles()
+
     return (
       <React.Fragment>
         <ElevationScroll>
             <AppBar position="fixed">
-                <Toolbar>
-                  <Typography variant="h3">
-                    2-Slice-Gaming
-                  </Typography>
+                <Toolbar disableGutters>
+                  <img src={logo} alt="logo" className={classes.logo} />
                 </Toolbar>
             </AppBar>
         </ElevationScroll>
