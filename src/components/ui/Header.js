1e12c7d87547d6dbc1fe4266c920500dda1a5bfc
joey <doode1524@gmail.com>
2021-04-24 16:29:31 -0400

    drawer Lists setup

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -13,7 +13,9 @@ import useMediaQuery from "@material-ui/core/useMediaQuery";
 import { useTheme } from "@material-ui/core/styles";
 import SwipeableDrawer from '@material-ui/core/SwipeableDrawer'
 import MenuIcon from '@material-ui/icons/Menu'
-
+import List from '@material-ui/core/List'
+import ListItem from'@material-ui/core/ListItem'
+import ListItemText from'@material-ui/core/ListItemText'
 import logo from "../../assets/logo.svg";
 import { IconButton } from "@material-ui/core";
 
@@ -276,7 +278,26 @@ const Header = (props) => {
   const drawer = (
     <React.Fragment>
       <SwipeableDrawer disableBackdropTransition={!iOS} disableDiscovery={iOS} open={openDrawer} onClose={() => setOpenDrawer(false)} onOpen={() => setOpenDrawer(true)}>
-        Example
+        <List disablePadding>
+          <ListItem onClick={() => setOpenDrawer(false)} divider button component={Link} to='/'>
+            <ListItemText disableTypography>Home</ListItemText>
+          </ListItem>
+          <ListItem onClick={() => setOpenDrawer(false)} divider button component={Link} to='/services'>
+            <ListItemText disableTypography>Services</ListItemText>
+          </ListItem>
+          <ListItem onClick={() => setOpenDrawer(false)} divider button component={Link} to='/revolution'>
+            <ListItemText disableTypography>The Revolution</ListItemText>
+          </ListItem>
+          <ListItem onClick={() => setOpenDrawer(false)} divider button component={Link} to='/about'>
+            <ListItemText disableTypography>About Us</ListItemText>
+          </ListItem>
+          <ListItem onClick={() => setOpenDrawer(false)} divider button component={Link} to='/contact'>
+            <ListItemText disableTypography>Contact Us</ListItemText>
+          </ListItem>
+          <ListItem onClick={() => setOpenDrawer(false)} divider button component={Link} to='/estimate'>
+            <ListItemText disableTypography>Free Estimate</ListItemText>
+          </ListItem>
+        </List>
       </SwipeableDrawer>
       <IconButton className={classes.drawerIconContainer} onClick={() => setOpenDrawer(!openDrawer)} disableRipple>
         <MenuIcon className={classes.drawerIcon}/>
