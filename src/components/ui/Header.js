7b0725fdd3650bd22a5a335df2e1b9d1e28936c5
joey <doode1524@gmail.com>
2021-04-24 01:57:25 -0400

    menus setup

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,48 +1,50 @@
-import React, { useState, useEffect } from 'react'
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
-import useScrollTrigger from '@material-ui/core/useScrollTrigger';
-import { makeStyles } from '@material-ui/styles'
-import Tabs from "@material-ui/core/Tabs"
-import Tab from "@material-ui/core/Tab"
-import Button from "@material-ui/core/Button"
-import { Link } from "react-router-dom"
+import React, { useState, useEffect } from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import useScrollTrigger from "@material-ui/core/useScrollTrigger";
+import { makeStyles } from "@material-ui/styles";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Button from "@material-ui/core/Button";
+import { Link } from "react-router-dom";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
 
-import logo from '../../assets/logo.svg'
+import logo from "../../assets/logo.svg";
 
 function ElevationScroll(props) {
-    const { children } = props;
-    const trigger = useScrollTrigger({
-      disableHysteresis: true,
-      threshold: 0,
-    });
-  
-    return React.cloneElement(children, {
-      elevation: trigger ? 4 : 0,
-    });
-  }
+  const { children } = props;
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+  });
 
-const useStyles = makeStyles(theme => ({
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+}
+
+const useStyles = makeStyles((theme) => ({
   toolbarMargin: {
     ...theme.mixins.toolbar,
-    marginBottom: "3em"
+    marginBottom: "3em",
   },
   logo: {
-    height: "8em"
+    height: "8em",
   },
   logoContainer: {
     padding: 0,
     "&:hover": {
-      backgroundColor: "transparent"
-    }
+      backgroundColor: "transparent",
+    },
   },
   tabContainer: {
-    marginLeft: 'auto'
+    marginLeft: "auto",
   },
   tab: {
     ...theme.typography.tab,
     minWidth: 10,
-    marginLeft: "25px"
+    marginLeft: "25px",
   },
   button: {
     ...theme.typography.estimate,
@@ -51,70 +53,123 @@ const useStyles = makeStyles(theme => ({
     marginRight: "25px",
     height: "45px",
   },
-}))
+}));
 
 const Header = (props) => {
-  const classes = useStyles()
-  const [value, setValue] = useState(0)
+  const classes = useStyles();
+  const [value, setValue] = useState(0);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [open, setOpen] = useState(false);
 
   const handleChange = (e, value) => {
-    setValue(value)
-  }
+    setValue(value);
+  };
 
-  useEffect(() => { // persists route upon refresh
+  const handleClick = (e) => {
+    setAnchorEl(e.currentTarget);
+    setOpen(true);
+  };
+
+  const handleClose = (e) => {
+    setAnchorEl(null);
+    setOpen(false);
+  };
+
+  useEffect(() => {
+    // persists route upon refresh
     if (window.location.pathname === "/" && value !== 0) {
-      setValue(0)
+      setValue(0);
     } else if (window.location.pathname === "/services" && value !== 1) {
-      setValue(1)
+      setValue(1);
     } else if (window.location.pathname === "/revolution" && value !== 2) {
-      setValue(2)
-    } else if(window.location.pathname === "/about" && value !== 3) {
-      setValue(3)
-    } else if(window.location.pathname === "/contact" && value !== 4) {
-      setValue(4)
-    } else if(window.location.pathname === "/estimate" && value !== 5) {
-      setValue(5)
-    } 
-  }, [value]) // only when value updates
+      setValue(2);
+    } else if (window.location.pathname === "/about" && value !== 3) {
+      setValue(3);
+    } else if (window.location.pathname === "/contact" && value !== 4) {
+      setValue(4);
+    } else if (window.location.pathname === "/estimate" && value !== 5) {
+      setValue(5);
+    }
+  }, [value]); // only when value updates
 
-    return (
-      <React.Fragment>
-        <ElevationScroll>
-            <AppBar position="fixed">
-                <Toolbar disableGutters>
-                  <Button 
-                    component={Link} to="/" 
-                    className={classes.logoContainer} 
-                    onClick={() => setValue(0)} 
-                    disableRipple
-                  >
-                    <img src={logo} alt="logo" className={classes.logo} />
-                  </Button>
-                  <Tabs 
-                    value={value} 
-                    onChange={handleChange} 
-                    className={classes.tabContainer}
-                    // indicatorColor="primary" // removes bar from selected button
-                  >
-                    <Tab className={classes.tab} component={Link} to="/" label="Home"></Tab>
-                    <Tab className={classes.tab} component={Link} to="/services" label="Services"></Tab>
-                    <Tab className={classes.tab} component={Link} to="/revolution" label="The Revolution"></Tab>
-                    <Tab className={classes.tab} component={Link} to="/about" label="About Us"></Tab>
-                    <Tab className={classes.tab} component={Link} to="/contact" label="Contact Us"></Tab>
-                  </Tabs>
-                  <Button 
-                    variant="contained" 
-                    color="secondary"
-                    className={classes.button}
-                  >
-                      Free Estimate
-                  </Button>
-                </Toolbar>
-            </AppBar>
-        </ElevationScroll>
-        <div className={classes.toolbarMargin} />
-      </React.Fragment>    
-    )
-}
+  return (
+    <React.Fragment>
+      <ElevationScroll>
+        <AppBar position="fixed">
+          <Toolbar disableGutters>
+            <Button
+              component={Link}
+              to="/"
+              className={classes.logoContainer}
+              onClick={() => setValue(0)}
+              disableRipple
+            >
+              <img src={logo} alt="logo" className={classes.logo} />
+            </Button>
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              className={classes.tabContainer}
+              indicatorColor="primary" // removes bar from selected button
+            >
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/"
+                label="Home"
+              ></Tab>
+              <Tab
+                aria-owns={anchorEl ? "simple-menu" : undefined}
+                aria-haspopup={anchorEl ? "true" : undefined}
+                className={classes.tab}
+                component={Link}
+                onMouseOver={(event) => handleClick(event)}
+                to="/services"
+                label="Services"
+              ></Tab>
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/revolution"
+                label="The Revolution"
+              ></Tab>
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/about"
+                label="About Us"
+              ></Tab>
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/contact"
+                label="Contact Us"
+              ></Tab>
+            </Tabs>
+            <Button
+              variant="contained"
+              color="secondary"
+              className={classes.button}
+            >
+              Free Estimate
+            </Button>
+            <Menu
+              id="simple-menu"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              MenuListProps={{onMouseLeave: handleClose}} // removes onMouseOver
+            >
+              <MenuItem onClick={handleClose}>Custom Software</MenuItem>
+              <MenuItem onClick={handleClose}>Mobile App Development</MenuItem>
+              <MenuItem onClick={handleClose}>Website Development</MenuItem>
+            </Menu>
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
+      <div className={classes.toolbarMargin} />
+    </React.Fragment>
+  );
+};
 
-export default Header
+export default Header;
