1409d2f11bae1c5a5863d3a640afae71ba2e06cc
joey <doode1524@gmail.com>
2021-04-23 20:03:22 -0400

    presist current route on page refresh

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
@@ -28,7 +28,13 @@ const useStyles = makeStyles(theme => ({
     marginBottom: "3em"
   },
   logo: {
-    height: "7em"
+    height: "8em"
+  },
+  logoContainer: {
+    padding: 0,
+    "&:hover": {
+      backgroundColor: "transparent"
+    }
   },
   tabContainer: {
     marginLeft: 'auto'
@@ -44,7 +50,7 @@ const useStyles = makeStyles(theme => ({
     marginLeft: "50px",
     marginRight: "25px",
     height: "45px",
-  }
+  },
 }))
 
 const Header = (props) => {
@@ -55,12 +61,35 @@ const Header = (props) => {
     setValue(value)
   }
 
+  useEffect(() => { // persists route upon refresh
+    if (window.location.pathname === "/" && value !== 0) {
+      setValue(0)
+    } else if (window.location.pathname === "/services" && value !== 1) {
+      setValue(1)
+    } else if (window.location.pathname === "/revolution" && value !== 2) {
+      setValue(2)
+    } else if(window.location.pathname === "/about" && value !== 3) {
+      setValue(3)
+    } else if(window.location.pathname === "/contact" && value !== 4) {
+      setValue(4)
+    } else if(window.location.pathname === "/estimate" && value !== 5) {
+      setValue(5)
+    } 
+  }, [value]) // only when value updates
+
     return (
       <React.Fragment>
         <ElevationScroll>
             <AppBar position="fixed">
                 <Toolbar disableGutters>
-                  <img src={logo} alt="logo" className={classes.logo} />
+                  <Button 
+                    component={Link} to="/" 
+                    className={classes.logoContainer} 
+                    onClick={() => setValue(0)} 
+                    disableRipple
+                  >
+                    <img src={logo} alt="logo" className={classes.logo} />
+                  </Button>
                   <Tabs 
                     value={value} 
                     onChange={handleChange} 
