f9aac2f2b96244407c7344d53552f3795447b34a
joey <doode1524@gmail.com>
2021-04-24 23:06:14 -0400

    footer links grid setup

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
 import footerAdornment from "../../assets/Footer Adornment.svg";
 
 const useStyles = makeStyles((theme) => ({
@@ -7,7 +8,7 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: theme.palette.common.blue,
     width: "100%",
     zIndex: 1302, // clip appbar to footer max
-    position: "relative"
+    position: "relative",
   },
   adornment: {
     width: "25em",
@@ -19,6 +20,15 @@ const useStyles = makeStyles((theme) => ({
       width: "15em",
     },
   },
+  mainContainer: {
+    position: "absolute",
+  },
+  link: {
+    color: "white",
+    fontFamily: "Arial",
+    fontSize: "0.75rem",
+    fontWeight: "bold",
+  },
 }));
 
 export default function Footer() {
@@ -26,6 +36,67 @@ export default function Footer() {
 
   return (
     <footer className={classes.footer}>
+      <Grid container justify="center" className={classes.mainContainer}>
+        <Grid item>
+          <Grid container direction="column">
+            <Grid item className={classes.link}>
+              Home
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item>
+          <Grid container direction="column">
+            <Grid item className={classes.link}>
+              Services
+            </Grid>
+            <Grid item className={classes.link}>
+              Custom Software Development
+            </Grid>
+            <Grid item className={classes.link}>
+              Mobile App Development
+            </Grid>
+            <Grid item className={classes.link}>
+              Website Development
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item>
+          <Grid container direction="column">
+            <Grid item className={classes.link}>
+                The Revolution
+            </Grid>
+            <Grid item className={classes.link}>
+                Vision
+            </Grid>
+            <Grid item className={classes.link}>
+                Technology
+            </Grid>
+            <Grid item className={classes.link}>
+                Process
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item>
+            <Grid container direction="column">
+                <Grid item className={classes.link}>
+                    About Us
+                </Grid>
+                <Grid item className={classes.link}>
+                    Histoy
+                </Grid>
+                <Grid item className={classes.link}>
+                    Team
+                </Grid>
+            </Grid>
+        </Grid>
+        <Grid item>
+            <Grid container direction="column">
+                <Grid item className={classes.link}>
+                    Contact Us
+                </Grid>
+            </Grid>
+        </Grid>
+      </Grid>
       <img
         alt="black decorative slash"
         src={footerAdornment}
