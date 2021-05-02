6542147e19ed16759fba65a82e3413666c67df02
joey <doode1524@gmail.com>
2021-05-01 22:12:52 -0400

    revolution card style

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -13,6 +13,7 @@ import animationData from "../animations/landinganimation/data";
 import customSoftwareIcon from "../assets/Custom Software Icon.svg";
 import mobileAppsIcon from "../assets/mobileIcon.svg";
 import websitesIcon from "../assets/websiteIcon.svg";
+import revolutionBackground from "../assets/repeatingBackground.svg";
 
 const useStyles = makeStyles((theme) => ({
   animation: {
@@ -88,6 +89,20 @@ const useStyles = makeStyles((theme) => ({
       padding: 25,
     },
   },
+  revolutionBackground: {
+    backgroundImage: `url(${revolutionBackground})`,
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    height: "100%",
+    width: "100%",
+  },
+  revolutionCard: {
+    position: "absolute",
+    boxShadow: theme.shadows[10],
+    borderRadius: 15,
+    padding: "10em"
+  }
 }));
 
 export default function LandingPage() {
@@ -268,29 +283,44 @@ export default function LandingPage() {
         </Grid>
       </Grid>
       <Grid item>
-        <Card>
-          <CardContent>
-            <Grid container direction="column" style={{textAlign: "center"}}>
-              <Grid item>
-                <Typography variant="h3">The Revolution</Typography>
+        <Grid
+          container
+          style={{ height: "100em" }}
+          alignItems="center"
+          justify="center"
+        >
+          <Card className={classes.revolutionCard}>
+            <CardContent>
+              <Grid
+                container
+                direction="column"
+                style={{ textAlign: "center" }}
+              >
+                <Grid item>
+                  <Typography variant="h3">The Revolution</Typography>
+                </Grid>
+                <Grid item>
+                  <Typography variant="subtitle1">
+                    Visionary insights coupled with cutting-edge technology is a
+                    recipe for revolution.
+                  </Typography>
+                  <Button
+                    variant="outlined"
+                    className={classes.learnButtonHero}
+                  >
+                    <span style={{ marginRight: 10 }}>Learn More</span>
+                    <ButtonArrow
+                      width={15}
+                      height={15}
+                      fill={theme.palette.common.blue}
+                    />
+                  </Button>
+                </Grid>
               </Grid>
-              <Grid item>
-                <Typography variant="subtitle1">
-                  Visionary insights coupled with cutting-edge technology is a
-                  recipe for revolution.
-                </Typography>
-                <Button variant="outlined" className={classes.learnButtonHero}>
-                  <span style={{ marginRight: 10 }}>Learn More</span>
-                  <ButtonArrow
-                    width={15}
-                    height={15}
-                    fill={theme.palette.common.blue}
-                  />
-                </Button>
-              </Grid>
-            </Grid>
-          </CardContent>
-        </Card>
+            </CardContent>
+          </Card>
+          <div className={classes.revolutionBackround} />
+        </Grid>
       </Grid>
     </Grid>
   );
