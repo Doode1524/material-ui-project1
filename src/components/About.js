1f2e478636a6287aad428131cbf4239580672ef9
joey <doode1524@gmail.com>
2021-05-09 16:51:30 -0400

    about us page - team

--- src/components/About.js
+++ src/components/About.js
@@ -126,6 +126,64 @@ export default function About(props) {
           </Grid>
         </Grid>
       </Grid>
+      <Grid
+        item
+        container
+        direction="column"
+        alignItems="center"
+        className={classes.rowContainer}
+      >
+        <Grid item>
+          <Typography align="center" variant="h4" gutterBottom>
+            Team
+          </Typography>
+        </Grid>
+        <Grid item>
+          <Typography variant="body1" paragraph align="center">
+            Zachary Reece, Founder
+          </Typography>
+          <Typography variant="body1" paragraph align="center">
+            I started coding when I was 9 years old.
+          </Typography>
+        </Grid>
+        <Grid item>
+          <Avatar alt="founder" src={profile} className={classes.avatar} />
+        </Grid>
+        <Grid item container>
+          <Grid item container direction="column" lg>
+            <Grid item>
+              <img src={yearbook} alt="yearbook page about founder" />
+            </Grid>
+            <Grid item>
+              <Typography variant="caption">
+                a page from my Sophomore yearbook
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid item lg style={{maxWidth: "45em", padding: "1.25em"}}>
+            <Typography variant="body1" align="center" paragraph>
+              I taught myself basic coding from a library book in third grade,
+              and ever since then my passion has solely been set on learning —
+              learning about computers, learning mathematics and philosophy,
+              studying design, always just learning.
+            </Typography>
+            <Typography variant="body1" align="center" paragraph>
+              Now I’m ready to apply everything I’ve learned, and to help others
+              with the intuition I have developed.
+            </Typography>
+          </Grid>
+          <Grid item container direction="column" lg alignItems="flex-end">
+            <Grid item>
+              <img src={puppy} alt="grey spotted puppy" />
+            </Grid>
+            <Grid item>
+              <Typography variant="caption">
+                my miniature dapple dachshund, Sterling
+              </Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
     </Grid>
   );
 }
