e0ed92c6ad3a1fb5451dedc5d119899bad4f166f
joey <doode1524@gmail.com>
2021-05-10 17:09:31 -0400

    contact page initial setup

---
+++ src/components/Contact.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+// import axios from "axios";
+import { Link } from "react-router-dom";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import Dialog from "@material-ui/core/Dialog";
+import DialogContent from "@material-ui/core/DialogContent";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import Snackbar from "@material-ui/core/Snackbar";
+
+import ButtonArrow from "./ui/ButtonArrow";
+
+import background from "../assets/background.jpg";
+import mobileBackground from "../assets/mobileBackground.jpg";
+import phoneIcon from "../assets/phone.svg";
+import emailIcon from "../assets/email.svg";
+import airplane from "../assets/send.svg";
+
+const useStyles = makeStyles((theme) => ({
+  background: {
+    backgroundImage: `url(${background})`,
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    height: "60em",
+  },
+}));
+
+export default function Contact() {
+  const classes = useStyles();
+  const theme = useTheme();
+
+  return (
+    <Grid container direction="row">
+      <Grid item container direction="column" lg={3}>
+        <Grid item>
+          <Typography variant="h2" style={{ lineHeight: 1 }}>
+            Contact Us
+          </Typography>
+          <Typography
+            variant="body1"
+            style={{ color: theme.palette.common.blue }}
+          >
+            We're waiting.
+          </Typography>
+        </Grid>
+      </Grid>
+      <Grid item container className={classes.background} lg={9}></Grid>
+    </Grid>
+  );
+}
